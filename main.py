ba84125db77ed7b7f7e05339d7a90c4856a30ce0
CBinyenya <calbinyex@gmail.com>
2018-01-03 12:00:47 +0300

    Changed functionality

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! from flask1'
+
+@app.route('/flask1')
+def flask1():
+	return 'I am flask 1'
+
+@app.route('/greeting')
+def greeting():
+	r = requests.get('http://flask2/greeting')	
+	return r.text
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',8008)
+    app.run('0.0.0.0',5000)
