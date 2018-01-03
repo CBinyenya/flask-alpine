16d7c620b4f5e70c43e7aaf8671defbf67d8f3be
CBinyenya <calbinyex@gmail.com>
2018-01-03 12:23:30 +0300

    Changed ports

--- Dockerfile
+++ Dockerfile
@@ -37,4 +37,4 @@ VOLUME /tmp
 
 CMD python /foo/main.py
 
-EXPOSE 5000
+EXPOSE 80
--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def greeting():
 	return r.text
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',5000)
+    app.run('0.0.0.0',80)
